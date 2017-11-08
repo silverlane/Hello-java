85731b12c4be3d69a3e6a7c63aa401a796b80fcb
silverlane <sush.cse034@gmail.com>
2017-11-08 17:40:28 +0530

    Update HelloServlet.java

--- src/main/java/org/cloudfoundry/samples/HelloServlet.java
+++ src/main/java/org/cloudfoundry/samples/HelloServlet.java
@@ -27,7 +27,7 @@ public class HelloServlet extends HttpServlet {
 			ip_addr = ssh_client_info.substring(portEnd + 1, dockerIPAddressEnd);
 		}
 
-		writer.println("Hello !! from"+ip_addr);		
+		writer.println("Hello !! from "+ip_addr);		
 		writer.close();
 	}
 }
